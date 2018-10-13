a580ab0a3ce6b77a5a8b05c75b3fd191c9efc40b
InshalFatima <38943108+InshalFatima@users.noreply.github.com>
2018-10-13 17:31:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// // write your JS code here
+
+function add() {
+    let txtNumber = document.getElementById("txtNumber");
+    let newNumber = parseInt(txtNumber.value) + 1;
+    txtNumber.value = newNumber;
+}
+
+function subtract() {
+    let txtNumber = document.getElementById("txtNumber");
+    let newNumber = parseInt(txtNumber.value) - 1;
+    txtNumber.value = newNumber;
+}
+
+
+
+
+
